f6c8437bd781b014ce3b9169926e02f6122f55b6
Ghazaleh <ghazaleh.mng1@gmail.com>
2021-05-23 23:35:24 +0430

    push my edit css

--- assets/js/mySlider.js
+++ assets/js/mySlider.js
@@ -3,7 +3,7 @@ var slideIndex = 0;
         
         function showSlides() {
           var i;
-          var slides = document.getElementsByClassName("mySlides");
+          var slides = document.getElementsByClassName("mainSlider");
           var dots = document.getElementsByClassName("dot");
           for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";  
